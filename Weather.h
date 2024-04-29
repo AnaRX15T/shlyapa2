669199871603536c7d459ace7d70d403cb31cfce
User_Sanya <bavk228@mail.ru>
2024-04-29 09:51:01 +0300

    it works

--- Weather.h
+++ Weather.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 class Weather
 {
 	std::string city;
@@ -11,5 +12,6 @@ class Weather
 	int clouds;
 public:
 	inline Weather(std::string cit, double lo, double la, double temperat, std::string weath, double wind, int cloud) { city = cit; lon = lo; lat = la; temperature = temperat; weather = weath; windSpeed = wind; clouds = cloud; };
+	inline void printWeather() { std::cout << "City: " << city << '\n' << "Lon: " << lon << '\n' << "Lat: " << lat << '\n' << "Temperature: " << temperature << '\n' << "Weather: " << weather << '\n' << "Wind speed: " << windSpeed << '\n' << "Clouds: " << clouds << '\n'; }
 };
 
